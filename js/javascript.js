8672b3fefd21065e6f522b0c3a3312d4dbbe34c9
Nicholas Finney <nickfinney@Nicholass-MacBook-Pro.local>
2019-05-14 18:39:32 -0700

    timer class created and working

---
+++ js/javascript.js
@@ -0,0 +1,2 @@
+const sessionTimer = new Timer(document.querySelector('#session-timer').parentNode);
+const breakTimer = new Timer(document.querySelector('#break-timer').parentNode);
---
+++ js/timer.js
@@ -0,0 +1,37 @@
+class Timer {
+
+    constructor(node){
+        // declarations
+        this.node = node;
+        this.time = parseInt(node.querySelector('.timer').innerHTML);
+        this.timerText = node.querySelector('.timer');
+        this.increaseButton = node.querySelector('.increase-time');
+        this.decreaseButton = node.querySelector('.decrease-time');
+
+        // event listeners
+        this.increaseButton.addEventListener("click", evt => this.increaseTime(evt));
+        this.decreaseButton.addEventListener("click", evt => this.decreaseTime(evt));
+        
+    }
+
+    increaseTime(e) {
+        if(this.time >= 30) {
+            this.time = 30;
+        } else {
+            this.time += 1;
+        }
+        this.displayTime();
+    }
+    decreaseTime(e) {
+        if(this.time <= 0) {
+            this.time = 0;
+        } else {
+            this.time -= 1;
+        }
+        this.displayTime();
+    }
+
+    displayTime() {
+        this.timerText.innerHTML = this.time;
+    }
+}
